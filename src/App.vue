6954b415c4aea012fa3fdbcf9de8b24855e55dcb
nolliebigspin <alec007@t-online.de>
2019-06-04 13:55:44 +0200

    Teaser fast fertig

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div
     id="app"
     class="container"
@@ -8,21 +9,30 @@
       title="Der EMI-Award - MeetUp der digitalen Generation"
       description="Hier treffen sich zukünftige Systemarchitekten, Spieleentwickler, Webdesigner, Medienpsychologen und UX-Experten."
     />
+    <Teaser
+      motto="Beyond Surface"
+      date="2019"
+    />
   </div>
 </template>
 
 <script>
 const MenuBar = () => import("./components/MenuBar/MenuBar");
 const Hero = () => import("./components/Hero/Hero");
+const Teaser = () => import("./components/Teaser/Teaser");
 
 export default {
   name: 'App',
   components: {
     MenuBar,
-    Hero
+    Hero,
+    Teaser
   }
 }
 </script>
 
 <style scoped>
+.container {
+  background: #eeeeee;
+}
 </style>
